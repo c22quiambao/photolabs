f9a9b108a103c624379debfce856e925e22cbfc0
c22quiambao <christelquiambao@gmail.com>
2024-01-16 19:19:08 -0800

    cleaned up code in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+//code for App.jsx
 import React, { useState } from "react";
 
 //import PhotoListItem from "./components/PhotoListItem";
@@ -7,45 +8,8 @@ import TopNavigation from "components/TopNavigationBar";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-	//const [selectedItems, setSelectedItems] = useState([]);
-
-	/*const handleToggleFavorite = (itemId) => {
-    setSelectedItems((prevSelectedItems) => {
-      if (prevSelectedItems.includes(itemId)) {
-        return prevSelectedItems.filter((id) => id !== itemId);
-      } else {
-        return [...prevSelectedItems, itemId];
-      }
-    });
-  };*/
-
-	/*const sampleDataForPhotoListItem = {
-		id: "1",
-		location: {
-			city: "Montreal",
-			country: "Canada",
-		},
-		imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-		username: "Joe Example",
-		profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-	};*/
-
-	//const photos = new Array(3).fill(sampleDataForPhotoListItem);
-
 	return (
 		<div className="App">
-			{/*{photos.map((element) => {
-				console.log("element being read", element);
-				return (
-					<PhotoListItem
-						key={element.id}
-						{...element}
-					/>
-				);
-			})}*/}
-			{/* solution of LHL
-      { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) }
-			 //*/}
 			<TopNavigation />
 			<PhotoList />
 		</div>
