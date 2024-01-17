9edf40bb674b2b05f86f006f816cb19d85fe1e18
c22quiambao <christelquiambao@gmail.com>
2024-01-16 19:37:31 -0800

    fixed HomeRoute issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@
 import React, { useState } from "react";
 
 //import PhotoListItem from "./components/PhotoListItem";
-import PhotoList from "components/PhotoList";
+//import PhotoList from "components/PhotoList";
 import "./App.scss";
-import TopNavigation from "components/TopNavigationBar";
+//import TopNavigation from "components/TopNavigationBar";
+import HomeRoute from "routes/HomeRoute.jsx";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 	return (
 		<div className="App">
-			<TopNavigation />
-			<PhotoList />
+			<HomeRoute />
 		</div>
 	);
 };
